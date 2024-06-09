17889a4b0bf27fe368ad222acc64960ef05e5a37
hyunminhwan <hmh3319@gmail.com>
2024-06-09 14:52:33 +0900

    [level 0] Title: 문자열의 앞의 n글자, Time: 0.03 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/0/181907. 문자열의 앞의 n글자/문자열의 앞의 n글자.java
+++ 프로그래머스/0/181907. 문자열의 앞의 n글자/문자열의 앞의 n글자.java
@@ -1,7 +1,7 @@
 class Solution {
     public String solution(String my_string, int n) {
         String answer = "";
-        
+        //substring 사용하여 문자 불러오기
          answer = my_string.substring(0,n);
         return answer;
     }
