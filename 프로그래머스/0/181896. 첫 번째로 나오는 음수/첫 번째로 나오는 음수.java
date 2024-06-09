29e5f7d9f7772fa68caa8019c9d3809164da7bb6
hyunminhwan <hmh3319@gmail.com>
2024-06-09 14:50:46 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.03 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.java
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.java
@@ -1,12 +1,15 @@
 class Solution {
     public int solution(int[] num_list) {       
         int answer = 0;
-        
+        //for문으로 배열에 있는 숫자 가져오기
         for(int i=0;i<num_list.length;i++){
+            //if문으로 배열안에 있는값중 음수 찾기
             if(num_list[i]<0){
+                //음수 인 배열 index 찾아서 answer에 ibdex 번호 넣기
                 answer=i;
                 break;
             }else{
+                //전부 양수면 -1 리턴
             answer=-1;
         }
         }
