8fe87026c6526024c745ad04e7eabb1f0de9eb73
hyunminhwan <hmh3319@gmail.com>
2024-06-12 18:27:14 +0900

    [level 0] Title: 수 조작하기 2, Time: 641.90 ms, Memory: 396 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -1,9 +1,10 @@
 class Solution {
     public String solution(int[] numLog) {
         String answer = "";
-        
+        //for문으로 numLog에 들어가는 숫자 두개를 빼서 입력값받기
         for(int i=0;i<numLog.length-1;i++){
             int a = (numLog[i+1]-numLog[i]);
+            //받은 입력값을 switch  문에 넣어서 조작키값 받기
             switch(a){
                 case 1 : 
                     answer += "w"; 
