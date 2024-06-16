3229baa2004cb31db744581832447f2586856f59
hyunminhwan <hmh3319@gmail.com>
2024-06-16 21:11:06 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.03 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
@@ -1,7 +1,7 @@
 class Solution {
     public int solution(int[] array, int height) {
         int answer = 0;
-        int count =0;
+        //for문을 이용하여 머쓱이의 키보다 큰사람을 찾는다
         for(int i=0;i<array.length;i++){
            if(array[i]>height){
                answer++;
