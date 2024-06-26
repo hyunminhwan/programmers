42496f3d6c306b04c2d8c90f310f1d8ff49862f6
hyunminhwan <hmh3319@gmail.com>
2024-06-26 14:54:55 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 19.27 ms, Memory: 87.3 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
@@ -1,20 +1,23 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] arr, int[][] queries) {
-        
+        //answer int[] 선언
         int[] answer = new int[queries.length];
+        //for문으로 queries에 있는 열에있는 수 꺼내기
         for(int i=0; i<queries.length;i++){
         ArrayList<Integer> min =new  ArrayList<Integer>();
             int[] num =queries[i];
+            //수 각각 넣기
             int no1 = num[0];
             int no2 = num[1];
             int no3 = num[2];
-            
+            //k보다 큰수 찾기
             for(int j=no1;j<=no2;j++){
                 if(arr[j]>no3){
                     min.add(arr[j]);
                 }
             }
+            //k보다 큰수 & 그중에서 작은수 찾고 아무것도 없으면 -1리턴
             if(min.size()>0){
             Collections.sort(min);
              answer[i]=min.get(0); 
