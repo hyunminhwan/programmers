7dc1a3bd6422a7e67aad09ccdcacaa9511b6acbd
hyunminhwan <hmh3319@gmail.com>
2024-07-01 20:24:24 +0900

    [level 0] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.02 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.java
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.java
@@ -6,9 +6,8 @@ class Solution {
            //if 문으로 n의 짝수와 i의 짝수 조건으로 제곱의 합 구하기
            if(n%2==0 && i%2==0){
                answer += i*i;
-               
             //n의 홀수와 i의 홀수 조건으로 홀수합 구하기
-           }else if (n%2!=0 && i%2!=0){
+           }else if (n%2==1 && i%2==1){
                answer += i;
            }
        }
