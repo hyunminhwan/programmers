56dc50c2c23c8f68bb077af063adc92b12214a78
hyunminhwan <hmh3319@gmail.com>
2024-07-01 20:42:39 +0900

    [level 0] Title: 배열 만들기 1, Time: 1.45 ms, Memory: 70.1 MB -BaekjoonHub

--- 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
@@ -1,13 +1,13 @@
 class Solution {
    
     public int[] solution(int n, int k) {
-          int count = 0;
-        for (int i = 1; i <= n; i++) {
-            if (i % k == 0) {
-                count++;
-            }
-        }
-        int[] answer = new int[count];
+        //   int count = 0;
+        // for (int i = 1; i <= n; i++) {
+        //     if (i % k == 0) {
+        //         count++;
+        //     }
+        // }
+        int[] answer = new int[n/k];
         int index = 0;
         for (int i = 1; i <= n; i++) {
             if (i % k == 0) {
