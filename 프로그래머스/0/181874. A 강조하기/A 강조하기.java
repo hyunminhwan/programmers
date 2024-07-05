3f7b61df7e9f4e9100e28c836e5c809f247fe4e0
hyunminhwan <hmh3319@gmail.com>
2024-07-05 18:28:30 +0900

    [level 0] Title: A 강조하기, Time: 0.03 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+            myString= myString.toLowerCase();
+        return answer=myString.replace("a","A");
+    }
+}
