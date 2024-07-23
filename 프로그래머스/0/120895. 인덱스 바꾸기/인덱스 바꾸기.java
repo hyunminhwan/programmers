67f1181616f560c07c381821d39dabe9ea9f045d
hyunminhwan <hmh3319@gmail.com>
2024-07-23 09:41:07 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 13.40 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        String answer = "";
+        char str1 = my_string.charAt(num1);
+        char str2 = my_string.charAt(num2);
+        for(int i=0;i<my_string.length();i++){
+            if(i==num1){
+            answer+=str2 ;
+            }else if(i==num2){
+            answer+= str1;
+            }else{
+            answer+=my_string.charAt(i);
+            }
+        }
+        return answer;
+        
+    }
+}
