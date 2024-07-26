04eb80ba14c548f84d3ecbdfdb05aeed9e68991b
hyunminhwan <hmh3319@gmail.com>
2024-07-26 19:36:32 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.04 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder(my_string);
+        sb.reverse();
+        return answer=sb.toString();
+    }
+}
