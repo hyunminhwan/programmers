546d272db60cb4d3c7a9209d20646bb9bc63ed46
hyunminhwan <hmh3319@gmail.com>
2024-08-22 00:34:32 +0900

    [level 0] Title: 정수를 나선형으로 배치하기, Time: 0.06 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
+++ 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
@@ -1,35 +1,35 @@
 class Solution {
     public int[][] solution(int n) {
         int[][] answer =new int[n][n];
-        int rowstart = 0;
-        int rowend=n-1;
-        int colstart = 0;
-        int colend = n-1;
+        int start = 0;
+        int end=n-1;
+        int start2 = 0;
+        int end2 = n-1;
         int i=0;
-        int no=1;
-        while(i<n*n){
-            for(int j=rowstart; j<=rowend;j++){
-                answer[rowstart][j]=no++;
+        int no=0;
+        while(i<=n*n){
+            for(int j=start; j<=end;j++){
+                answer[start][j]=no++;
                 i++;
             }
-            rowstart++;
+            start++;
             
-            for(int j=rowstart;j<=colend;j++){
-                answer[j][colend]=no++;
+            for(int j=start;j<=end;j++){
+                answer[j][end]=no++;
                 i++;
             }
-            colend--;
+            end--;
             
-            for(int j=colend;j>=colstart;j--){
-                answer[rowend][j]=no++;
+            for(int j=end;j>=start2;j--){
+                answer[end2][j]=no++;
                 i++;
             }
-            rowend--;
-            for(int j=rowend;j>=rowstart;j--){
-               answer[j][colstart]=no++;
+            end2--;
+            for(int j=end2;j>start2;j--){
+               answer[j][start2]=no++;
                 i++;
             }
-            colstart++;
+            start2++;
         }
         
         
@@ -38,10 +38,11 @@ class Solution {
 }
 //숫자는 1~ n*n
 //n n-1 n-1 n-2 n-2 ...1 1 될때 까지
-//[0][0] [0][1] [0][2] [0][3] 가로
-//[1][3] [2][3] [3][3] 세로
-//[3][2] [3][1] [3][0] 가로
-//[2][0] [1][0] 세로
+//[0][0] [0][1] [0][2] [0][3] 
+//[1][3] [2][3] [3][3] 
+//[3][2] [3][1] [3][0] 
+
+//[2][0] [1][0] 
 
 //[1][1] [1][2]
 //[2][2]
